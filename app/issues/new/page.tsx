f4d2556239ac3c955276a681bb3e2fd53c3a2dd4
Khalid Ali <khalludi123@gmail.com>
2023-11-12 20:16:03 -0500

    Handle unexpected error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,53 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { red } from "@radix-ui/colors";
+
+interface NewIssue {
+  title: string;
+  description: string;
+}
 
 export default function NewIssuesPage() {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<NewIssue>();
+  const [error, setError] = useState("");
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root>
-        <TextField.Input placeholder={"Title"} />
-      </TextField.Root>
-      <SimpleMDE placeholder="Description" />
-      <Button>Submit New Issue</Button>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="max-w-xl space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (e) {
+            setError("An unexpected error has occurred");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder={"Title"} {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name={"description"}
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
     </div>
   );
 }
